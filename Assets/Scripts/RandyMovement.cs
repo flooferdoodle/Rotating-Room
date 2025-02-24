96233c590d52a767fead42e7c159175245bb3f42
strandie <138831899+strandie@users.noreply.github.com>
2025-02-23 23:23:09 -0500

    changes to angle

--- Assets/Scripts/RandyMovement.cs
+++ Assets/Scripts/RandyMovement.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class RandyMovement : MonoBehaviour
+public class RandyMovement3D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Movement speed in units per second
+    public float moveSpeed = 5f;
+    private Vector3 upDirection = new Vector3(1, 0, 1).normalized; private Vector3 downDirection = new Vector3(-1, 0, -1).normalized;
+    private Vector3 leftDirection = new Vector3(-1, 0, 1).normalized; private Vector3 rightDirection = new Vector3(1, 0, -1).normalized;
 
-    // Update is called once per frame
-    void Update()
+    // Camera that sprite tracks
+    public Camera mainCamera;
+
+    // Find camera
+    /*void Start()
     {
-        
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }*/
+
+    // Using FixedUpdate for smooth physics-based movement
+    void FixedUpdate()
+    {
+        Vector3 moveDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveDir += upDirection;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveDir += downDirection;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveDir += leftDirection;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveDir += rightDirection;
+        }
+
+        // Only move if a key is pressed
+        if (moveDir != Vector3.zero)
+        {
+            moveDir.Normalize();
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
     }
 }
